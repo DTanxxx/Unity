dd10dfef3dd2ae4bc710716f300c2502ab8a63b0
David Tan <>
2021-01-28 22:33:31 +0800

    Re Added Glitch Garden

---
+++ Unity2D/Glitch Garden/Assets/Scripts/Shooter.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+    GameObject projectileParent;
+    const string PROJECTILE_PARENT_NAME = "Projectiles";
+
+    private void Start()
+    {
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
+        CreateProjectileParent();
+    }
+
+    private void CreateProjectileParent()
+    {
+        projectileParent = GameObject.Find(PROJECTILE_PARENT_NAME);
+        if (!projectileParent)
+        {
+            projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
+        }
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            animator.SetBool("IsAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("IsAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            // check if the difference in y positions (the absolute value) is smaller than the smallest number (Mathf.Epsilon)
+            bool IsCloseEnough =
+                (Mathf.Abs(spawner.transform.position.y - transform.position.y)
+                <= Mathf.Epsilon);
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    // this method is called from Event system in animation
+    public void Fire()
+    {
+        GameObject newProjectile =
+            Instantiate(projectile, gun.transform.position, transform.rotation)
+            as GameObject;
+        newProjectile.transform.parent = projectileParent.transform;
+    }
+}
