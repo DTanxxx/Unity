29f9e084614e0b0916f41cb36cd7e7a65ef5c668
David Tan <>
2021-01-28 22:43:58 +0800

    Re Added everything

---
+++ Unity2D/Space Shooter/Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 100;
+    int damageDueToShipCollision = 200;
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    public void Hit()
+    {
+        if (damage != damageDueToShipCollision)
+        {
+            Destroy(gameObject);
+        }       
+    }
+}
---
+++ Unity2D/TileVania/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int pointsForCoinPickup = 100;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject player = collision.gameObject as GameObject;
+        CapsuleCollider2D playerBody = player.GetComponent<CapsuleCollider2D>();
+        if (!playerBody.IsTouchingLayers(LayerMask.GetMask("Interactables")))
+        {
+            return;
+        }
+
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+        FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
+        Destroy(gameObject);
+    }
+}
